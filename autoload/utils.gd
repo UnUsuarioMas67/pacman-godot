c6f7be78e390fe1aac0c7440b53fbb879539312b
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-12 17:18:49 -0400

    created utils.gd autoload

---
+++ autoload/utils.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func is_direction_free(
+		body: Node2D, 
+		move_speed:float, 
+		shape_query_params: PhysicsShapeQueryParameters2D, 
+		direction: Vector2, 
+		delta: float
+) -> bool:
+	shape_query_params.transform = body.global_transform.translated(move_speed * direction * delta * 2)
+	var result = body.get_world_2d().direct_space_state.intersect_shape(shape_query_params)
+	return result.size() == 0
