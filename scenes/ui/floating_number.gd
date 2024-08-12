d5796b2d110625b8e2d1a48b5f89a4a803af1a21
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-12 17:18:59 -0400

    Add FloatingNumber scene

---
+++ scenes/ui/floating_number.gd
@@ -0,0 +1,25 @@
+extends Node2D
+class_name FloatingNumber
+
+const FLOATING_NUMBER = preload("res://scenes/ui/floating_number.tscn")
+
+var _value: int = 0
+
+@onready var label: Label = $Label
+
+
+static func create(parent: Node2D, value: int):
+	var floating_number: FloatingNumber = FLOATING_NUMBER.instantiate()
+	floating_number._value = value
+	floating_number.global_position = parent.global_position
+	parent.add_child(floating_number)
+
+
+func _ready() -> void:
+	top_level = true
+	label.text = str(_value)
+	
+	var tween: Tween = create_tween()
+	tween.tween_property(self, "position:y", position.y - 4, .2)
+	tween.tween_callback(queue_free)
+	
