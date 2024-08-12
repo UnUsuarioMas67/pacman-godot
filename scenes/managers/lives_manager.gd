256c3bb35fcc92f93f921e0fa5f46226eaa42c63
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-12 17:18:57 -0400

    Add argument to lives_updated signal in LivesManager

--- scenes/managers/lives_manager.gd
+++ scenes/managers/lives_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name LivesManager
 
-signal lives_updated
+signal lives_updated(new_lives: int)
 
 @export var starting_lives: int = 3
 
@@ -15,4 +15,4 @@ func _ready() -> void:
 
 func _on_player_death() -> void:
 	lives = max(lives - 1, 0)
-	lives_updated.emit()
+	lives_updated.emit(lives)
