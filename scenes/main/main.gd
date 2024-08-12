3450109868e1527f0e0e821025168ee324e8a2a8
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-12 17:18:56 -0400

    Add function to reload the level

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const LEVEL_SCENE: PackedScene = preload("res://scenes/level/level.tscn")
+
 @onready var level: Level = $Level
 
 
@@ -7,7 +9,7 @@ func _ready():
 	GameEvents.level_completed.connect(
 		func():
 			await get_tree().create_timer(0.5).timeout
-			get_tree().reload_current_scene()
+			reload_level()
 	)
 	
 	level.play_intro()
@@ -19,3 +21,16 @@ func _ready():
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("reset_game"):
 		get_tree().reload_current_scene()
+
+
+func reload_level() -> void:
+	remove_child(level)
+	level.queue_free()
+	
+	var new_level = LEVEL_SCENE.instantiate()
+	add_child(new_level)
+	
+	new_level.play_intro()
+	new_level.stopped.connect(func():
+		level.play_intro()
+	)
