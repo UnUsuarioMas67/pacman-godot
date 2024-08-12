4bbe3f86fb2b18272ae760addc059f5d32c0845c
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-12 17:18:56 -0400

    Add LivesManager scene and script

---
+++ scenes/managers/lives_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+class_name LivesManager
+
+signal lives_updated
+
+@export var starting_lives: int = 3
+
+var lives: int
+
+
+func _ready() -> void:
+	GameEvents.player_death_finished.connect(_on_player_death)
+	lives = starting_lives
+
+
+func _on_player_death() -> void:
+	lives = max(lives - 1, 0)
+	lives_updated.emit()
