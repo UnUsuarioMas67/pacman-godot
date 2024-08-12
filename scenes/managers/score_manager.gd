d3a14715aebbdb5424f43144b59c8d9830e432e3
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-12 17:18:56 -0400

    Add Score Manager scene

---
+++ scenes/managers/score_manager.gd
@@ -0,0 +1,38 @@
+extends Node
+class_name ScoreManager
+ 
+signal score_updated(new_score: int)
+
+const PILL_VALUE: int = 10
+const BIG_PILL_VALUE: int = 50
+const GHOST_COMBO_VALUES: Array[int] = [200, 400, 800, 1600]
+
+var score: int = 0
+var ghost_combo: int = 0
+
+
+func _ready() -> void:
+	GameEvents.pill_collected.connect(_on_pill_collected)
+	GameEvents.big_pill_collected.connect(_on_big_pill_collected)
+	GameEvents.ghost_eaten.connect(_on_ghost_eaten)
+	GameEvents.global_ghost_state_updated.connect(_on_global_ghost_state_updated)
+
+
+func _on_pill_collected() -> void:
+	score += PILL_VALUE
+	score_updated.emit(score)
+
+
+func _on_big_pill_collected() -> void:
+	score += BIG_PILL_VALUE
+	score_updated.emit(score)
+
+
+func _on_ghost_eaten() -> void:
+	score += GHOST_COMBO_VALUES[ghost_combo]
+	ghost_combo = (ghost_combo + 1) % GHOST_COMBO_VALUES.size()
+	score_updated.emit(score)
+
+
+func _on_global_ghost_state_updated(_global_state: Ghost.State, _scared_mode: bool) -> void:
+	ghost_combo = 0
