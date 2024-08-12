8f08b99758a77ce500c31e04dc16d12994fc9fba
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-12 17:18:58 -0400

    Make hud.tscn life counter functional

--- scenes/ui/hud.gd
+++ scenes/ui/hud.gd
@@ -1,20 +1,32 @@
 extends VBoxContainer
 
+const LIFE_SCENE: PackedScene = preload("res://scenes/ui/life.tscn")
+
 @export var score_manager: ScoreManager
 @export var lives_manager: LivesManager
 
 @onready var score_value: Label = %ScoreValue
-@onready var lives: HBoxContainer = $PanelContainer3/Lives
+@onready var lives_container: HBoxContainer = %Lives
 
 
 func _ready() -> void:
 	score_manager.score_updated.connect(_on_score_updated)
 	lives_manager.lives_updated.connect(_on_lives_updated)
+	
+	await lives_manager.ready
+	for life in lives_container.get_children():
+		life.queue_free()
+	
+	for life in lives_manager.starting_lives:
+		var life_instance = LIFE_SCENE.instantiate()
+		lives_container.add_child(life_instance)
 
 
 func _on_score_updated(new_score: int) -> void:
 	score_value.text = "%07d" % new_score
 
 
-func _on_lives_updated(new_lives: int) -> void:
-	pass
+func _on_lives_updated(lives_left: int) -> void:
+	# loop through lives_container children in reverse order
+	for life in range(lives_container.get_child_count() - 1, lives_left - 1, -1):
+		lives_container.get_child(life).hide()
