1522a00ec18cdf5d2cdba5406a67df9d5e2513f4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-13 18:49:45 -0400

    Display high score in the TitleScreen

--- scenes/title_screen/title_screen.gd
+++ scenes/title_screen/title_screen.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+@onready var high_score_value: Label = %HighScoreValue
+
+
+func _ready() -> void:
+	high_score_value.text = "%07d" % UserData.save_data["high_score"]
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("start"):
