518c2c9b4b55a57351780f6f91fcebc9d0c99415
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-13 18:56:45 -0400

    Make 'Press Enter to start' text blink

--- scenes/title_screen/title_screen.gd
+++ scenes/title_screen/title_screen.gd
@@ -1,10 +1,16 @@
 extends CanvasLayer
 
 @onready var high_score_value: Label = %HighScoreValue
+@onready var press_start: HBoxContainer = %PressStart
 
 
 func _ready() -> void:
 	high_score_value.text = "%07d" % UserData.save_data["high_score"]
+	
+	# make press start text blink
+	var tween: Tween = create_tween().set_loops()
+	tween.tween_property(press_start, "visible", true, 0).set_delay(0.2)
+	tween.tween_property(press_start, "visible", false, 0).set_delay(0.2)
 
 
 func _unhandled_input(event: InputEvent) -> void:
